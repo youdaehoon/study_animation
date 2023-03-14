e0d27c18138fb170c39103dc614f19d358f6667c
Dae Hoon YOU <sjssmsqkqh1@gmail.com>
2023-03-14 11:52:47 +0900

    #5 feat: pages/onload footer animation

--- src/pages/onload/index.tsx
+++ src/pages/onload/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import OnloadFooter from "./footer";
 import OnloadShowCard from "./showcard";
 
 const PageOnload = () => {
   return (
     <div style={{ backgroundColor: "gray", height: "500px" }}>
       <OnloadShowCard />
+      <OnloadFooter />
     </div>
   );
 };
