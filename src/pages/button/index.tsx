9f9e1495c000f71f434a7b215f9a11d3ea5ac09a
Dae Hoon YOU <sjssmsqkqh1@gmail.com>
2023-04-19 12:31:59 +0900

    feat:animation button

---
+++ src/pages/button/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./index.css";
+
+const PageButton = () => {
+  return (
+    <div>
+      <section className="button-container">
+        <div></div>
+        <div></div>
+        <div></div>
+      </section>
+    </div>
+  );
+};
+
+export default PageButton;
